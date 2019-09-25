61ebb84986147a4e80f047bc58c4c909a027841d
joshrs1199 <46760607+joshrs1199@users.noreply.github.com>
2019-09-25 16:32:11 -0400

    added the about me paragraph

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,7 +3,12 @@ import React from 'react'
 const AboutMe = () => {
   return(
     <div>
-      <h1>about me</h1>
+      <p>
+        I attended the highschool Bronx Academy for Software Engineering and there is where I picked up programming.
+        At first I did it because I had to but then it started becoming something more than just a grade I was getting.
+        Now years later, I am still very passionate about coding with a focus in Full Stack web development.
+        I value community in a workplace which is why I think the ideal workplace for me would be a small startup I can build and grow with.
+      </p>
     </div>
   )
 }
