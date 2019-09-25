bf760c69d1032f5b832c6200d9f6e66db82cfeff
joshrs1199 <46760607+joshrs1199@users.noreply.github.com>
2019-09-24 23:45:25 -0400

    working links hooked up to navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './App.css';
+import NavBar from './NavBar'
+import AboutMe from './components/AboutMe'
+import Resume from './components/Resume'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import { Route, Switch } from 'react-router-dom'
 
-class App extends React.Component {
-  
-  render() {
+const App = () => {
     return(
       <div>
+        <NavBar />
         <h1>Josh</h1>
+        <Switch>
+          <Route path='/aboutme' component={AboutMe}/>
+          <Route path='/resume' component={Resume}/>
+          <Route path='/projects' component={Projects}/>
+          <Route path='/contact' component={Contact}/>
+        </Switch>
       </div>
     )
-  }
 }
 
 export default App;
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,2 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
+const NavBar = () => {
+  return(
+    <div>
+      <Link to='/aboutme'>About Me</Link>
+      <Link to='/resume'>Resume</Link>
+      <Link to='/projects'>Projects</Link>
+      <Link to='/contact'>Contact</Link>
+    </div>
+  )
+}
+
+export default NavBar;
